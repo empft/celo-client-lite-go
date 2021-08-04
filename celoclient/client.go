5fb41037a7dfa21764cd111486117817b6d4f478
stevealexrs <59494379+stevealexrs@users.noreply.github.com>
2021-08-04 13:37:57 +0800

    Update Client

--- celoclient/client.go
+++ celoclient/client.go
@@ -11,19 +11,12 @@ import (
 	"gitlab.com/stevealexrs/celo-client-lite-go/contractkit/celotoken"
 )
 
-type Client interface {
-	Balance(string) (*celotoken.Balance, error)
-	SendRawTransaction(string) (common.Hash, error)
-	WaitForTransaction(common.Hash, time.Duration) (*types.Receipt, error)
-	LatestBlock() (*big.Int, error)
-}
-
-type client struct {
+type Client struct {
 	Connection *connection.Client
-	Kit		   *contractkit.Kit
+	Kit        *contractkit.Kit
 }
 
-func New(url string) (Client, error) {
+func New(url string) (*Client, error) {
 	conn, err := connection.New(url)
 	if err != nil {
 		return nil, err
@@ -34,24 +27,23 @@ func New(url string) (Client, error) {
 		return nil, err
 	}
 
-	return &client{Connection: conn, Kit: kit}, nil
+	return &Client{Connection: conn, Kit: kit}, nil
 }
 
 // Get the balance of all tokens including native currency token
-func (c *client) Balance(address string) (*celotoken.Balance, error) {
+func (c *Client) Balance(address string) (*celotoken.Balance, error) {
 	return c.Kit.Balance(address)
 }
 
 // Send hex encoded signed transaction payload and returns the hash
-func (c *client) SendRawTransaction(rawTx string) (common.Hash, error) {
+func (c *Client) SendRawTransaction(rawTx string) (common.Hash, error) {
 	return c.Connection.SendRawTransaction(rawTx)
 }
 
 // Wait for the transaction to complete by polling the blockchain every 500ms
-func (c *client) WaitForTransaction(txHash common.Hash, timeout time.Duration) (*types.Receipt, error) {
+func (c *Client) WaitForTransaction(txHash common.Hash, timeout time.Duration) (*types.Receipt, error) {
 	return c.Connection.WaitForTransaction(txHash, timeout)
 }
-func (c *client) LatestBlock() (*big.Int, error) {
+func (c *Client) LatestBlock() (*big.Int, error) {
 	return c.Connection.LatestBlock()
 }
-
